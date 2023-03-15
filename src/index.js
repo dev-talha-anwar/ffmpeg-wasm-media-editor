982ee4f55fee4be7fb7b2838a20263f0431b7b8d
dev-talha-anwar <786anwartalha@gmail.com>
2023-03-15 13:10:14 +0500

    added exit function

--- src/index.js
+++ src/index.js
@@ -34,11 +34,9 @@ export default class FFMPEGWasmMediaEditor {
   }
 
   async init( inputFile, fileName){
-    if(!this.ffmpeg.isLoaded){
-      console.log('Loading ffmpeg-core.js');
-      await this.ffmpeg.load();
-      console.log('Loading Input File');
-    }
+    console.log('Loading ffmpeg-core.js');
+    await this.ffmpeg.load();
+    console.log('Loading Input File');
     this.inputFile = inputFile;
     this.fileName = fileName;
     this.ffmpeg.FS('writeFile', fileName, await fetchFile(inputFile));
